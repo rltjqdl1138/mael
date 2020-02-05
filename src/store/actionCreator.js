11f8c64dbb7a6cc44c4c6f310ca2529133387e21
rltjqdl1138 <rltjqdl1138@naver.com>
2020-02-05 22:14:39 +0900

    [Add] store and actions for theme menu

--- src/store/actionCreator.js
+++ src/store/actionCreator.js
@@ -10,8 +10,10 @@ const {dispatch} = store
 import * as athenticationActions from './modules/athentication'
 import * as sidebarActions from './modules/sidebar'
 import * as mostlyActions from './modules/mostly'
+import * as themeActions from './modules/theme'
 
 // bind actions
 export const AthenticationActions=bindActionCreators(athenticationActions, dispatch)
 export const SidebarActions=bindActionCreators(sidebarActions, dispatch)
 export const MostlyActions=bindActionCreators(mostlyActions, dispatch)
+export const ThemeActions=bindActionCreators(themeActions, dispatch)
--- src/store/modules/index.js
+++ src/store/modules/index.js
@@ -2,9 +2,11 @@ import { combineReducers } from 'redux'
 import athentication from './athentication'
 import sidebar from './sidebar'
 import mostly from './mostly'
+import theme from './theme'
 
 export default combineReducers({
     athentication,
     sidebar,
-    mostly
+    mostly,
+    theme
 })
